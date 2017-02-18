7cd51c5bb5b1a518fda792935b7541d4a015f2b4
Monica Jones <monicaj314@yahoo.com>
2017-02-18 05:32:39 -0600

    added code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,62 @@
-//generate a random number in the answer div
-var rightAnswer = Math.floor(Math.random() * (120 - 19) + 1) + 19;
-$("#answer").text(rightAnswer);
-console.log(rightAnswer);
-
 var crystalGame = {
+	rightAnswer: Math.floor(Math.random() * (120 - 19) + 1) + 19,
 	wins: 0,
 	losses: 0,
-	total: 0,
 	guessNumber: 0,
+	gameOver: false,
+  	message: "",
 	newGame: function(){
-	}
+    this.rightAnswer = Math.floor(Math.random() * (120 - 19) + 1) + 19;
+    $("#answer").text(this.rightAnswer);
+    this.guessNumber = 0;
+    this.updateScore();
+    this.gameOver = false;
+  },
+  updateScore: function() {
+    $("#message").text(this.message);
+    $("#guess").text("Your Number: " + this.guessNumber);
+    $("#wins").text("Wins: " + this.wins);
+    $("#losses").text("Losses: " + this.losses);
+  }
 	
 }
+crystalGame.newGame();
+
 //give each crystal a random number value
-var crystal = [{'name':"blue",'value':0}, {'name':"red",'value':0}, {'name':"rainbow",'value':0}, {'name':"clear",'value':0}];
+var crystal = [{name:"blue",value:0}, {name:"red",value:0}, {name:"rainbow",value:0}, {name:'clear',value:0}];
 for (var i = 0; i < crystal.length; i++ ) {
 	var guess = (Math.floor(Math.random() * 12) + 1);
-  	crystal[i].value = guess;	
-	console.log(guess);
+  	crystal[i].value = guess;
+  	console.log(guess);
+
+//apply click function to all crystal divs
+  $('#'+ crystal[i].name).click(function(){
+  	var crystalColor = $(this).attr('id');
+  	var crystalObject = crystal.find(function(color) {
+  		if (color.name == crystalColor) {
+  			return color;
+  		}
+  	})
+  	crystalGame.guessNumber += crystalObject.value; 
+  	$('#guess').html("Your Number: " + crystalGame.guessNumber);
+
+
+  	if (crystalGame.guessNumber === crystalGame.rightAnswer) {
+  		crystalGame.wins++;
+  		crystalGame.message = "YOU WIN! :D Click anywhere to play again.";
+      crystalGame.updateScore();
+  		crystalGame.gameOver=true;
+  	} else if (crystalGame.guessNumber > crystalGame.rightAnswer) {
+  		crystalGame.losses++;
+  		crystalGame.message = "YOU LOSE! D: Click anywhere to play again.";
+      	crystalGame.updateScore();
+  		crystalGame.gameOver=true;
+  	}
+  })
 }
-//update html with current numbers
-$("#guess").text("Your Number: " + crystalGame.guessNumber);
-$("#wins").text("Wins: " + crystalGame.wins);
-$("#losses").text("Losses: " + crystalGame.losses);
-
-
-
+	
+$('button').click(function() {
+	if (crystalGame.gameOver)  {
+		crystalGame.newGame();
+		}
+    })
