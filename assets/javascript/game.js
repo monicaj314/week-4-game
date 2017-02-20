bfe69603732bd10d13592a6ae04c26a4d3852423
Monica Jones <monicaj314@yahoo.com>
2017-02-20 14:53:17 -0600

    added some images and fixed crystal resetting numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var crystalGame = {
 
 	newGame: function(){
     this.rightAnswer = Math.floor(Math.random() * (120 - 19) + 1) + 19;
-    $("#answer").text(this.rightAnswer);
+    $("#answer").text("Goal Number: " + this.rightAnswer);
     this.message = "";
     this.guessNumber = 0;
     this.updateScore();
@@ -27,58 +27,59 @@ var crystalGame = {
   	 if (this.gameOver == true) {
   	 var resetButton = $('<button>Play Again</button>');
   	 $('#button-holder').html(resetButton);
-  	 } else {
+  	 } else { 
   	 	crystalGame.newGame();
   	 }
-  	}
-	
+  	},
+  	updateCrystalValues: function() {
+     for (var i = 0; i < crystal.length; i++ ) {
+		var guess = (Math.floor(Math.random() * 12) + 1);
+  		crystal[i].value = guess;
+  		console.log(guess);
+		}
+   }
 }
 crystalGame.newGame();
 
 //give each crystal a random number value
 var crystal = [{name:"blue",value:0}, {name:"red",value:0}, {name:"rainbow",value:0}, {name:'clear',value:0}];
+
 for (var i = 0; i < crystal.length; i++ ) {
-	var guess = (Math.floor(Math.random() * 12) + 1);
-  	crystal[i].value = guess;
-  	console.log(guess);
-
-//apply click function to all crystal divs
-  $('#'+ crystal[i].name).click(function(){
-  	if(crystalGame.gameOver) return
-  	var crystalColor = $(this).attr('id');
-  	var crystalObject = crystal.find(function(color) {
-  		if (color.name == crystalColor) {
-  			return color;
-  		}
-  	})
-  	crystalGame.guessNumber += crystalObject.value; 
-  	$('#guess').html("Your Number: " + crystalGame.guessNumber);
-
-
-
-
-  	if (crystalGame.guessNumber === crystalGame.rightAnswer) {
-  		crystalGame.wins++;
-  		crystalGame.message = 'OMG YOU DID IT! :D Click "Play Again" to reset.';
-      	crystalGame.updateScore();
-  		crystalGame.gameOver=true; 
-  		crystalGame.stopGame();
-  	} else if (crystalGame.guessNumber > crystalGame.rightAnswer) {
-  		crystalGame.losses++;
-  		crystalGame.message = 'Uh oh! Your number went over D: Click "Play Again" to reset.';
-      	crystalGame.updateScore();
-  		crystalGame.gameOver=true;
-  		crystalGame.stopGame();
+		var guess = (Math.floor(Math.random() * 12) + 1);
+  		crystal[i].value = guess;
+  		console.log(guess);
+	//apply click function to all crystal divs
+  		$('#'+ crystal[i].name).click(function(){
+  			if(crystalGame.gameOver) return
+  			var crystalColor = $(this).attr('id');
+  			var crystalObject = crystal.find(function(color) {
+  				if (color.name == crystalColor) {
+  				return color;
+  			}
+  		})
+  		crystalGame.guessNumber += crystalObject.value; 
+  		$('#guess').html("Your Number: " + crystalGame.guessNumber);
+		if (crystalGame.guessNumber === crystalGame.rightAnswer) {
+	  		crystalGame.wins++;
+	  		crystalGame.message = 'OMG YOU DID IT! :D Click "Play Again" to reset.';
+	      	crystalGame.updateScore();
+	  		crystalGame.gameOver=true; 
+	  		crystalGame.stopGame();
+  		} else if (crystalGame.guessNumber > crystalGame.rightAnswer) {
+	  		crystalGame.losses++;
+	  		crystalGame.message = 'Uh oh! Your number went over D: Click "Play Again" to reset.';
+	      	crystalGame.updateScore();
+	  		crystalGame.gameOver=true;
+	  		crystalGame.stopGame();
+	  	}
+	  })
   	}
-  })
-}
-
-	
 	$(document).on("click", 'button', function() {
 	if (crystalGame.gameOver)  {
-		crystalGame.newGame();
+			crystalGame.updateCrystalValues();
+  		crystalGame.newGame();
 	} else {
 		crystalGame.stopGame();
 		}
-
     })
+
